c8fad94d5116c40bd034e914a36b4345f826dc11
Sanikhsan12 <lordkazumasan12@gmail.com>
2024-10-14 09:24:18 +0700

    perubahan

--- loop.py
+++ loop.py
@@ -7,6 +7,7 @@ list_nama = ["ikhsan","akmal","tri"]
 i = 0
 
 # contoh penggunaan for
+# for i in range(angka awal perulangan , angka akhir perulangan , increment):
 for i in range(3):
     print(list_nama[i]) # print array berdasarkan indeks loop
 
@@ -14,3 +15,6 @@ for i in range(3):
 while i < 3 :
     print(list_nama[i])
     i+=1 # increment ( i = i + 1)
+
+
+
